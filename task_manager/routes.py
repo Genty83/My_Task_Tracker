f263c3cf2a3616a6a0127a516a1d52d91aeb411d
Craig Gent <genty83@outlook.com>
2024-06-13 18:31:18 +0000

    Added additional template functions

--- task_manager/routes.py
+++ task_manager/routes.py
@@ -1,10 +1,27 @@
 # IMPORTS
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from task_manager import app, db
-from task_manager.models import Catagory, Task
+from task_manager.models import Category, Task
 
 
 # HOMEPAGE
 @app.route("/")
 def home():
     return render_template("tasks.html")
+
+
+# CATEGORIES
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+
+# HOMEPAGE
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("add_category.html")
