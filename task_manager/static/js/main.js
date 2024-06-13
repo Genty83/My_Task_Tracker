402636b1ea2cb119fe5ab6aed13d13ece7f3f757
Craig Gent <genty83@outlook.com>
2024-06-13 19:45:11 +0000

    Added event listner

--- task_manager/static/js/main.js
+++ task_manager/static/js/main.js
@@ -3,3 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var sidebar = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidebar);
   });
+
+// Model
+document.addEventListener('DOMContentLoaded', function() {
+  var model = document.querySelectorAll('.modal');
+  M.Modal.init(model);
+})
